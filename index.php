2c4f93137bdc5149b7bf2f5479b97d09d1eafcb5
myquynh125 <quynhnguyentm9@gmail.com>
2022-03-30 20:29:45 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HTML</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header class="sticky-top">
+		<div class="container">
+			<div class="row">
+				<div class="col-2 menu">
+					<img src="uploads/logo.png">
+				</div>
+				<div class="col-2">
+					
+				</div>
+				<div class="col-8 menu">
+					<ul>
+						<li><a class="home" href="">Trang chủ</a></li>
+						<li><a class="home" href="">Sản phẩm</a>
+							<ul class="menu_child">
+								<li><a class="home" href="">Đồ gia dụng</a></li>
+								<li><a class="home" href="">Đồ điện tử</a></li>
+								<li><a class="home" href="">Đồ trang sức</a></li>
+							</ul>
+						</li>
+						<li><a class="home" href="">Liên hệ</a></li>
+						<li><a class="home" href="">Hướng dẫn</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</header>
+	<div class="container slider-margin">
+		<div class="row">
+			<div class="col-12">
+					<div class="slideshow-container">
+		<div class="mySlides fade">
+			<div class="numbertext">
+				1/3
+			</div>
+			<img src="uploads/logo1.png" style="width: 100%;">
+		</div>
+		<div class="mySlides fade">
+			<div class="numbertext">
+				2/3
+			</div>
+			<img src="uploads/logo2.png" style="width: 100%;">
+		</div>
+		<div class="mySlides fade">
+			<div class="numbertext">
+				3/3
+			</div>
+			<img src="uploads/logo3.png" style="width: 100%;">
+		</div>
+		<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+		<a class="next" onclick="plusSlides(1)">&#10095;</a>
+	</div>
+	<br>
+	<div style="text-align: center;">
+		<span class="dot" onclick="currentSlide(1)"></span>
+		<span class="dot" onclick="currentSlide(2)"></span>
+		<span class="dot"onclick="currentSlide(3)"></span>
+	</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 title">
+				<h3>Product</h3>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-2th">
+				<div class="card">
+					<div class="image">
+						<img src="uploads/12333.png">
+					</div>
+					<div class="name">
+						<p>SAMSUNG GALAXY</p>
+					</div>
+					<div class="price">
+						<p>23.000.000 VNĐ</p>
+					</div>
+				</div>
+			</div>
+						<div class="col-2th">
+				<div class="card">
+					<div class="image">
+						<img src="uploads/12333.png">
+					</div>
+					<div class="name">
+						<p>SAMSUNG GALAXY</p>
+					</div>
+					<div class="price">
+						<p>23.000.000 VNĐ</p>
+					</div>
+				</div>
+			</div>
+						<div class="col-2th">
+				<div class="card">
+					<div class="image">
+						<img src="uploads/12333.png">
+					</div>
+					<div class="name">
+						<p>SAMSUNG GALAXY</p>
+					</div>
+					<div class="price">
+						<p>23.000.000 VNĐ</p>
+					</div>
+				</div>
+			</div>
+						<div class="col-2th">
+				<div class="card">
+					<div class="image">
+						<img src="uploads/12333.png">
+					</div>
+					<div class="name">
+						<p>SAMSUNG GALAXY</p>
+					</div>
+					<div class="price">
+						<p>23.000.000 VNĐ</p>
+					</div>
+				</div>
+			</div>
+						<div class="col-2th">
+				<div class="card">
+					<div class="image">
+						<img src="uploads/12333.png">
+					</div>
+					<div class="name">
+						<p>SAMSUNG GALAXY</p>
+					</div>
+					<div class="price">
+						<p>23.000.000 VNĐ</p>
+					</div>
+				</div>
+			</div>
+						<div class="col-2th">
+				<div class="card">
+					<div class="image">
+						<img src="uploads/12333.png">
+					</div>
+					<div class="name">
+						<p>SAMSUNG GALAXY</p>
+					</div>
+					<div class="price">
+						<p>23.000.000 VNĐ</p>
+					</div>
+				</div>
+			</div>
+						<div class="col-2th">
+				<div class="card">
+					<div class="image">
+						<img src="uploads/12333.png">
+					</div>
+					<div class="name">
+						<p>SAMSUNG GALAXY</p>
+					</div>
+					<div class="price">
+						<p>23.000.000 VNĐ</p>
+					</div>
+				</div>
+			</div>
+						<div class="col-2th">
+				<div class="card">
+					<div class="image">
+						<img src="uploads/12333.png">
+					</div>
+					<div class="name">
+						<p>SAMSUNG GALAXY</p>
+					</div>
+					<div class="price">
+						<p>23.000.000 VNĐ</p>
+					</div>
+				</div>
+			</div>
+						<div class="col-2th">
+				<div class="card">
+					<div class="image">
+						<img src="uploads/12333.png">
+					</div>
+					<div class="name">
+						<p>SAMSUNG GALAXY</p>
+					</div>
+					<div class="price">
+						<p>23.000.000 VNĐ</p>
+					</div>
+				</div>
+			</div>
+						<div class="col-2th">
+				<div class="card">
+					<div class="image">
+						<img src="uploads/12333.png">
+					</div>
+					<div class="name">
+						<p>SAMSUNG GALAXY</p>
+					</div>
+					<div class="price">
+						<p>23.000.000 VNĐ</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer>
+		<div class="container">
+			<div class="row footer">
+				<div class="col-3">
+					<h3>THÔNG TIN CÔNG TY</h3>
+					<ul class="menu_footer">
+						<li><a href="">Giới thiệu công ty</a></li>
+						<li><a href="">Phương châm bán hàng</a></li>
+						<li><a href="">Mua hàng doanh nghiệp</a></li>
+						<li><a href="">Hệ thống siêu thị</a></li>
+					</ul>
+				</div>
+				<div class="col-3">
+					<h3>CHÍNH SÁCH CHUNG</h3>
+					<ul class="menu_footer">
+						<li><a href="">Bảo trì - Bảo hành - Đổi trả</a></li>
+						<li><a href="">Quy định giao hàng</a></li>
+						<li><a href="">Điều khoản sử dụng</a></li>
+						<li><a href="">Thoả thuận người dùng</a></li>
+					</ul>
+				</div>
+				<div class="col-3">
+					<h3>SẢN PHẨM</h3>
+					<ul class="menu_footer">
+						<li><a href="">ASUS</a></li>
+						<li><a href="">DELL</a></li>
+						<li><a href="">MAC</a></li>
+						<li><a href="">MSI</a></li>
+					</ul>
+				</div>
+				<div class="col-3">
+					<h3>MUA HÀNG ONLINE</h3>
+					<ul class="menu_footer">
+						<li><a href="">Thông tin chuyển khoản</a></li>
+						<li><a href="">Lợi ích mua hàng online</a></li>
+						<li><a href="">Hướng dẫn mua hàng</a></li>
+						<li><a href="">Câu hỏi thường gặp</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 copy_right">
+				<a href="">by quynhnguyentm9@gmail.com</a>
+			</div>
+		</div>
+	</footer>
+</body>
+<script>
+		var slideIndex = 1;
+		showSlides(slideIndex);
+
+		function plusSlides(n) {
+		  showSlides(slideIndex += n);
+		}
+
+		function currentSlide(n) {
+		  showSlides(slideIndex = n);
+		}
+
+		function showSlides(n) {
+		  var i;
+		  var slides = document.getElementsByClassName("mySlides");
+		  var dots = document.getElementsByClassName("dot");
+		  if (n > slides.length) {slideIndex = 1}    
+		  if (n < 1) {slideIndex = slides.length}
+		  for (i = 0; i < slides.length; i++) {
+		      slides[i].style.display = "none";  
+		  }
+		  for (i = 0; i < dots.length; i++) {
+		      dots[i].className = dots[i].className.replace(" active", "");
+		  }
+		  slides[slideIndex-1].style.display = "block";  
+		  dots[slideIndex-1].className += "active";
+		  setTimeout(showSlides, 1000);
+		}
+</script>
+</html>
